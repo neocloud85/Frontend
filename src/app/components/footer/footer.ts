3d29151a4d51dd87adabe05686387e2fd455bd6c
neocloud85 <neocloud85@gmail.com>
2026-05-25 17:52:20 +0200

    Add files via upload

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslatePipe } from '../../pipes/translate-pipe';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule, TranslatePipe, RouterModule],
   templateUrl: './footer.html',
   styleUrls: ['./footer.css']
 })
